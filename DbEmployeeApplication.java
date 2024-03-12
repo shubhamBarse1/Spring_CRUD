728c224739a9488229e4a4200f031b6af8c81238
Shubham Dayanand Barse <124044821+shubhamBarse1@users.noreply.github.com>
2024-03-12 12:04:07 +0530

    Add files via upload

---
+++ DbEmployeeApplication.java
@@ -0,0 +1,15 @@
+package com.example.DbEmployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbEmployeeApplication.class, args);
+		
+		System.out.println("Application is Started...");
+	}
+
+}
